9a54f7e8abede273aa8b00173a08a49b5f9e1da8
haley.philips <haley.philips@oakstreethealthcorp.com>
2025-07-14 00:17:16 -0700

    adjust css for larger viewports

--- src/App.vue
+++ src/App.vue
@@ -47,18 +47,4 @@ nav a {
 nav a:first-of-type {
   border: 0;
 }
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
